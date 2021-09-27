64e8bf293ec1331c53b1d1a8800f432521dc701b
_juliotati <59662912+Juliotati@users.noreply.github.com>
2021-09-27 15:51:01 +0100

    update app starting dimensions

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -29,7 +29,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(&run_loop, project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(800, 500);
+  Win32Window::Size size(600, 700);
   if (!window.CreateAndShow(L"Anime Watchlist", origin, size)) {
     return EXIT_FAILURE;
   }
