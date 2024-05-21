5a842f6977dadcf1873bd8bd2a42e6ab1b8fd06a
_juliotati <59662912+Juliotati@users.noreply.github.com>
2024-05-21 21:09:14 +0100

    Update anime_preview_provider.dart

--- lib/features/watchlist/presentation/provider/anime_preview_provider.dart
+++ lib/features/watchlist/presentation/provider/anime_preview_provider.dart
@@ -3,51 +3,9 @@ import 'dart:developer';
 import 'package:animewatchlist/core/core.dart';
 import 'package:animewatchlist/features/watchlist/data/datasources/remote_datasource.dart';
 import 'package:animewatchlist/features/watchlist/data/models/watchlist.dart';
-import 'package:animewatchlist/gen/assets.gen.dart';
+import 'package:animewatchlist/features/watchlist/presentation/presentation.dart' show AnimeState;
 import 'package:flutter/cupertino.dart';
 import 'package:injectable/injectable.dart';
-
-enum AnimeState {
-  error,
-  loading,
-  reloading,
-  ready,
-  empty;
-
-  const AnimeState();
-
-  bool get isLoading => this == loading;
-
-  bool get isReloading => this == reloading;
-
-  bool get showLoading => this == loading || this == reloading;
-
-  bool get isReady => this == ready;
-
-  bool get hasError => this == error;
-
-  bool get notData => this == empty;
-
-  String get stateMessage {
-    return switch (this) {
-      error => 'ERROR - COULD NOT LOAD WATCHLIST 🥲',
-      loading => 'LOADING...',
-      reloading => 'RELOADING...',
-      ready => 'READY',
-      empty => 'WATCHLIST IS EMPTY',
-    };
-  }
-
-  String get stateImage {
-    return switch (this) {
-      error => Assets.loadingGifs.luffySearch.path,
-      loading => Assets.loadingGifs.values.randomElement.path,
-      reloading => '',
-      ready => '',
-      empty => Assets.loadingGifs.luffySearch.path,
-    };
-  }
-}
 
 @lazySingleton
 final class AnimeProvider extends ChangeNotifier {
