ba4fa5b7d75e2d9695f87c7e168cfcaf3af7afce
_juliotati <59662912+Juliotati@users.noreply.github.com>
2024-05-21 21:09:18 +0100

    Create anime_state.dart

---
+++ lib/features/watchlist/presentation/provider/anime_state.dart
@@ -0,0 +1,43 @@
+part of '../presentation.dart';
+
+enum AnimeState {
+  error,
+  loading,
+  reloading,
+  ready,
+  empty;
+
+  const AnimeState();
+
+  bool get isLoading => this == loading;
+
+  bool get isReloading => this == reloading;
+
+  bool get showLoading => this == loading || this == reloading;
+
+  bool get isReady => this == ready;
+
+  bool get hasError => this == error;
+
+  bool get notData => this == empty;
+
+  String get stateMessage {
+    return switch (this) {
+      error => 'ERROR - COULD NOT LOAD WATCHLIST 🥲',
+      loading => 'LOADING...',
+      reloading => 'RELOADING...',
+      ready => 'READY',
+      empty => 'WATCHLIST IS EMPTY',
+    };
+  }
+
+  String get stateImage {
+    return switch (this) {
+      error => Assets.loadingGifs.luffySearch.path,
+      loading => Assets.loadingGifs.values.randomElement.path,
+      reloading => '',
+      ready => '',
+      empty => Assets.loadingGifs.luffySearch.path,
+    };
+  }
+}
