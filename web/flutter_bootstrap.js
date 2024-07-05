b1236242f4df32ecfc2d37577243d4ad8d3824ae
_juliotati <59662912+Juliotati@users.noreply.github.com>
2024-07-05 22:06:08 +0100

    Update flutter_bootstrap.js

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -1,17 +1,12 @@
 {{flutter_js}}
 {{flutter_build_config}}
 
-window.addEventListener('load', function (ev) {
-    _flutter.loader.loadEntrypoint({
-        serviceWorker: {
-            serviceWorkerVersion: {{flutter_service_worker_version}},
-        },
-        onEntrypointLoaded: function (engineInitializer) {
-            engineInitializer.initializeEngine({
-                useColorEmoji: true,
-            }).then(function (appRunner) {
-                appRunner.runApp();
-            });
-        }
-    });
+_flutter.loader.load({
+    serviceWorker: {
+        serviceWorkerVersion: {{flutter_service_worker_version}},
+    },
+    onEntrypointLoaded: async function(engineInitializer) {
+        const appRunner = await engineInitializer.initializeEngine({useColorEmoji: true});
+        await appRunner.runApp();
+    }
 });
