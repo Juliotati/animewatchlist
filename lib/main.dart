3d9e7dc860ffc9a56505b91183dfc4659335c689
_juliotati <59662912+Juliotati@users.noreply.github.com>
2024-09-18 21:44:06 +0100

    add SemanticsBinding

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:animewatchlist/core/config/di/app_di.dart';
 import 'package:animewatchlist/features/watchlist/presentation/provider/watchlist_provider.dart';
 import 'package:animewatchlist/features/watchlist/watchlist.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
 import 'package:link_target/link_target.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
   await AppConfig.initialize();
   runApp(const AnimeArchive());
+  SemanticsBinding.instance.ensureSemantics();
 }
 
 class AnimeArchive extends StatelessWidget {
